25a1a349b64392a13892e8533385ce9fac144f0b
alokehpdev <alok.regmi@ehp.dev>
2024-09-01 12:11:37 +0545

    Full screen like behavior and icons

--- app/src/main/java/com/olup/notable/views/FloatingEditorView.kt
+++ app/src/main/java/com/olup/notable/views/FloatingEditorView.kt
@@ -4,17 +4,17 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import com.olup.notable.EditorView
+import com.olup.notable.R
 import com.olup.notable.ui.theme.InkaTheme
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -24,6 +24,8 @@ fun FloatingEditorView(
     pageId: String,
     onDismissRequest: () -> Unit
 ) {
+    var isFullScreen by remember { mutableStateOf(false) } // State for full-screen mode
+
     Dialog(
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
@@ -31,7 +33,8 @@ fun FloatingEditorView(
         InkaTheme {
             Box(
                 modifier = Modifier
-                    .fillMaxSize(0.9f)
+                    .fillMaxHeight(if (isFullScreen) 1f else 0.98f) // Adjust height based on full-screen state
+                    .fillMaxWidth() // Ensure it fills the width
                     .background(Color.White)
             ) {
                 Column {
